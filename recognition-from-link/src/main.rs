869cc890d474501c6ab11d9b8b97d6787bba7bd1
wada <wadayugo@keio.jp>
2023-03-10 15:02:09 +0900

    download by external crate

--- recognition-from-link/src/main.rs
+++ recognition-from-link/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use youtube_downloader::download_movie;
+
+#[tokio::main]
+async fn main() {
+    let url = "https://www.youtube.com/watch?v=VFbhKZFzbzk";
+    // download
+    let video = download_movie(url).await.unwrap();
+    println!("Video title: {}", video.title);
 }
--- youtube-downloader/src/lib.rs
+++ youtube-downloader/src/lib.rs
@@ -1,6 +1,6 @@
-use youtube_dl::{Error, YoutubeDl};
+use youtube_dl::{Error, SingleVideo, YoutubeDl};
 
-pub async fn download_movie(url: &str) -> Result<(), Error> {
+pub async fn download_movie(url: &str) -> Result<SingleVideo, Error> {
     let output = YoutubeDl::new(url)
         .download(true)
         .socket_timeout("15")
@@ -9,12 +9,5 @@ pub async fn download_movie(url: &str) -> Result<(), Error> {
         .await
         .expect("Failed to get video information");
     let single_video = output.into_single_video().expect("Failed to download");
-    let title = single_video.title;
-    let duration = single_video.duration;
-    println!("Video title: {}", title);
-    // if duration is ok, print it
-    if let Some(duration) = duration {
-        println!("Video duration: {}", duration);
-    }
-    Ok(())
+    Ok(single_video)
 }
