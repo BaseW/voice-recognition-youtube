b89708f84fcd8687ed8c5f0e7aae787e756e425f
wada <wadayugo@keio.jp>
2023-03-12 00:30:53 +0900

    recognize from link

--- recognition-from-link/src/main.rs
+++ recognition-from-link/src/main.rs
@@ -1,13 +1,17 @@
+use hound::WavReader;
 use youtube_downloader::download_movie;
 
 #[tokio::main]
 async fn main() {
+    println!("downloading video...");
     let url = "https://www.youtube.com/watch?v=VFbhKZFzbzk";
     // download
     let video = download_movie(url).await.unwrap();
     println!("Video title: {}", video.title);
 
-    // convert to mp3 from webm
+    println!("converting to wav...");
+    let wav_path = "tmp/output.wav";
+    // convert to wav from webm
     let mut command = std::process::Command::new("ffmpeg");
     command
         .arg("-i")
@@ -19,10 +23,24 @@ async fn main() {
         .arg("2")
         .arg("-b:a")
         .arg("192k")
-        .arg("tmp/output.mp3");
+        .arg(wav_path);
     // print command
-    println!("command: {:?}", command);
+    // println!("command: {:?}", command);
     // execute command
     let output = command.spawn().expect("Failed to execute ffmpeg");
-    println!("output: {:?}", output);
+    // println!("output: {:?}", output);
+
+    println!("recognizing...");
+    // read path to vosk model from environment variable
+    let vosk_model_path = std::env::var("VOSK_MODEL_PATH").expect("VOSK_MODEL_PATH not set");
+    let mut vosk_recognizer = voice_recognition_vosk::VoskRecognizer::new(vosk_model_path, 44100);
+    let mut reader = WavReader::open(wav_path).expect("Could not create the WAV reader");
+    let samples = reader
+        .samples()
+        .collect::<hound::Result<Vec<i16>>>()
+        .expect("Could not read WAV file");
+    for sample in samples.chunks(100) {
+        vosk_recognizer.accept_waveform(sample);
+        println!("{:#?}", vosk_recognizer.partial_result());
+    }
 }
