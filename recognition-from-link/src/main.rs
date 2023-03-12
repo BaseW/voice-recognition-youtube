0dc5b91e641583135bb4340179efc2b295bf9412
wada <wadayugo@keio.jp>
2023-03-12 22:50:06 +0900

    refactor

--- recognition-from-link/src/main.rs
+++ recognition-from-link/src/main.rs
@@ -1,23 +1,12 @@
 use hound::WavReader;
 use youtube_downloader::download_movie;
 
-#[tokio::main]
-async fn main() {
-    println!("downloading video...");
-    let url = "https://www.youtube.com/watch?v=DZcNcQfEgnY";
-    // download
-    let video = download_movie(url).await.unwrap();
-    println!("Video title: {}", video.title);
-
-    println!("converting to wav...");
-    let wav_path = "tmp/output.wav";
-    let sample_rate = 44100;
-    // convert to wav from webm
+fn convert_file_by_ffmpeg(input_file_path: &str, output_file_path: &str, sample_rate: u32) {
     let mut command = std::process::Command::new("ffmpeg");
     command
         .arg("-y")
         .arg("-i")
-        .arg("tmp/video.webm")
+        .arg(input_file_path)
         .arg("-vn")
         .arg("-ar")
         .arg(format!("{}", sample_rate))
@@ -25,34 +14,37 @@ async fn main() {
         .arg("1")
         .arg("-b:a")
         .arg("192k")
-        .arg(wav_path);
+        .arg(output_file_path);
     // print command
     // println!("command: {:?}", command);
     // execute command
     // check status until done
     let status = command.status().expect("Failed to execute ffmpeg");
     println!("convert command exit status: {}", status);
+}
 
-    // split wav file into 10 seconds
+fn split_file_by_ffmpeg(input_file_path: &str, output_file_path: &str) {
     let mut command = std::process::Command::new("ffmpeg");
     command
         .arg("-y")
         .arg("-i")
-        .arg(wav_path)
+        .arg(input_file_path)
         .arg("-f")
         .arg("segment")
         .arg("-segment_time")
         .arg("10")
         .arg("-c")
         .arg("copy")
-        .arg("tmp/output%03d.wav");
+        .arg(output_file_path);
     // print command
     // println!("command: {:?}", command);
     // execute command
     // check status until done
     let status = command.status().expect("Failed to execute ffmpeg");
     println!("split command exit status: {}", status);
+}
 
+fn recognize_splitted_files(sample_rate: u32) {
     // read path to vosk model from environment variable
     let vosk_model_path = std::env::var("VOSK_MODEL_PATH").expect("VOSK_MODEL_PATH not set");
     let mut vosk_recognizer =
@@ -79,4 +71,26 @@ async fn main() {
             }
         }
     }
+}
+
+#[tokio::main]
+async fn main() {
+    println!("downloading video...");
+    let url = "https://www.youtube.com/watch?v=DZcNcQfEgnY";
+    // download
+    let video = download_movie(url).await.unwrap();
+    println!("Video title: {}", video.title);
+
+    println!("converting to wav...");
+    let input_file_path = "tmp/video.webm";
+    let output_file_path = "tmp/output.wav";
+    let sample_rate = 44100;
+    // convert to wav from webm
+    convert_file_by_ffmpeg(input_file_path, output_file_path, sample_rate);
+
+    // split wav file into 10 seconds
+    split_file_by_ffmpeg(output_file_path, "tmp/output%03d.wav");
+
+    // recognize splitted wav files
+    recognize_splitted_files(sample_rate);
 }
