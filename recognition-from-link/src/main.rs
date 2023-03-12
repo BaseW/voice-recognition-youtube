18ed25214be4d258699e59ac1ee36987fdb91b6d
wada <wadayugo@keio.jp>
2023-03-12 10:42:44 +0900

    use same vosk instance

--- recognition-from-link/src/main.rs
+++ recognition-from-link/src/main.rs
@@ -11,6 +11,7 @@ async fn main() {
 
     println!("converting to wav...");
     let wav_path = "tmp/output.wav";
+    let sample_rate = 44100;
     // convert to wav from webm
     let mut command = std::process::Command::new("ffmpeg");
     command
@@ -19,7 +20,7 @@ async fn main() {
         .arg("tmp/video.webm")
         .arg("-vn")
         .arg("-ar")
-        .arg("44100")
+        .arg(format!("{}", sample_rate))
         .arg("-ac")
         .arg("1")
         .arg("-b:a")
@@ -30,19 +31,46 @@ async fn main() {
     // execute command
     // check status until done
     let status = command.status().expect("Failed to execute ffmpeg");
-    println!("status: {}", status);
+    println!("convert command exit status: {}", status);
 
-    println!("recognizing...");
+    // split wav file into 10 seconds
+    let mut command = std::process::Command::new("ffmpeg");
+    command
+        .arg("-y")
+        .arg("-i")
+        .arg(wav_path)
+        .arg("-f")
+        .arg("segment")
+        .arg("-segment_time")
+        .arg("10")
+        .arg("-c")
+        .arg("copy")
+        .arg("tmp/output%03d.wav");
+    // print command
+    // println!("command: {:?}", command);
+    // execute command
+    // check status until done
+    let status = command.status().expect("Failed to execute ffmpeg");
+    println!("split command exit status: {}", status);
+
     // read path to vosk model from environment variable
     let vosk_model_path = std::env::var("VOSK_MODEL_PATH").expect("VOSK_MODEL_PATH not set");
-    let mut vosk_recognizer = voice_recognition_vosk::VoskRecognizer::new(vosk_model_path, 44100);
-    let mut reader = WavReader::open(wav_path).expect("Could not create the WAV reader");
-    let samples = reader
-        .samples()
-        .collect::<hound::Result<Vec<i16>>>()
-        .expect("Could not read WAV file");
-    for sample in samples.chunks(100) {
-        vosk_recognizer.accept_waveform(sample);
-        println!("{:#?}", vosk_recognizer.partial_result());
+    let mut vosk_recognizer =
+        voice_recognition_vosk::VoskRecognizer::new(vosk_model_path, sample_rate);
+
+    // recognize each files
+    // tmp/output001.wav, tmp/output002.wav, ...
+    for i in 1..=10 {
+        let wav_path = format!("tmp/output{:03}.wav", i);
+        println!("recognizing {}...", wav_path);
+        let mut reader = WavReader::open(wav_path).expect("Could not create the WAV reader");
+        let samples = reader
+            .samples()
+            .collect::<hound::Result<Vec<i16>>>()
+            .expect("Could not read WAV file");
+        for sample in samples.chunks(100) {
+            vosk_recognizer.accept_waveform(sample);
+            println!("{:#?}", vosk_recognizer.partial_result());
+        }
     }
 }
