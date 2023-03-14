ec330d973ecf539838b7771b1489d9da8c46fac3
BaseW <52400336+BaseW@users.noreply.github.com>
2023-03-15 00:06:11 +0900

    Issue 11: Youtube の検索用の関数を追加 (#13)
    * split functions into lib
    
    * update youtube_downnloader
    
    * update

--- youtube-downloader/src/lib.rs
+++ youtube-downloader/src/lib.rs
@@ -1,4 +1,4 @@
-use youtube_dl::{Error, SingleVideo, YoutubeDl};
+use youtube_dl::{Error, SearchOptions, SingleVideo, YoutubeDl};
 
 pub async fn download_movie(
     url: &str,
@@ -16,6 +16,45 @@ pub async fn download_movie(
         Err(err) => {
             println!("err: {:?}", err);
             Err(err)
+        }
+    }
+}
+
+pub async fn search_videos(search_query: String, count: usize) -> Vec<SingleVideo> {
+    let search_options = SearchOptions::youtube(search_query).with_count(count);
+    let youtube_dl = YoutubeDl::search_for(&search_options);
+    match youtube_dl.run() {
+        Ok(output) => match output.into_playlist() {
+            Some(playlist) => {
+                if let Some(entries) = playlist.entries {
+                    entries
+                } else {
+                    Vec::new()
+                }
+            }
+            None => Vec::new(),
+        },
+        Err(err) => {
+            println!("err: {:?}", err);
+            Vec::new()
+        }
+    }
+}
+
+pub async fn download_movie_by_id(id: &str, output_file_name: &str) -> Option<SingleVideo> {
+    let url = format!("https://www.youtube.com/watch?v={}", id);
+    match YoutubeDl::new(url)
+        .download(true)
+        .socket_timeout("15")
+        .output_directory(".")
+        .output_template(output_file_name)
+        .run_async()
+        .await
+    {
+        Ok(output) => output.into_single_video(),
+        Err(err) => {
+            println!("err: {:?}", err);
+            None
         }
     }
 }
